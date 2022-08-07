64d478966f31a76e6dd28bd83e2597d6db959b8d
dijon64 <john1.mclean@outlook.com>
2022-08-07 14:53:48 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function makeid() {
+
+    // setup to create new id
+    var newId = ''
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    var charsLen = chars.length
+
+    // generate new id
+    for (var i = 0; i < 36; i++) {
+        var rando = Math.floor(Math.random() * charsLen + 1)
+        newId += chars[rando]
+    }
+
+    return newId
+};
+
+// create list of tasks
+
+function addToList() {
+
+    // read task from seach bar
+    let newTask = document.getElementById('textBar').value
+
+    // create new task with id
+    var task = {
+        id: makeid(),
+        item: newTask,
+    }
+
+    let list = document.getElementById('taskList');
+
+    let li = document.createElement('li')
+    li.innerText = task['item']
+    list.appendChild(li)
+
+};
